4b770a83c34e470c4fd635a816fa2789122a3e97
Daniel Schell <schell.daniel@gmail.com>
2021-04-23 10:29:29 -0300

    Account Settings done

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -8,23 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  linkTheme = document.querySelector('#theme');
+  public linkTheme = document.querySelector('#theme');
+  public links: NodeListOf<Element>;
 
   constructor() { }
 
   ngOnInit(): void {
-  
+
+    this.links = document.querySelectorAll('.selector');
+    
     const url = localStorage.getItem('theme') || './assets/css/colors/default.css';
-    //document.querySelector('#theme').setAttribute('href',url);
     this.linkTheme.setAttribute('href',url);
+
+    this.checkCurrentTheme();
 
   }
 
   changeTheme(theme: string){
+
     const url=`./assets/css/colors/${theme}.css`;
-    //document.querySelector('#theme').setAttribute('href',url);
-    this.linkTheme.setAttribute('href',url);
-    
+
+    this.linkTheme.setAttribute('href',url);   
     localStorage.setItem('theme',url);
+    this.checkCurrentTheme();
+
+  }
+
+  checkCurrentTheme(){
+  
+    this.links.forEach(elem => {
+
+      elem.classList.remove('working');
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${ btnTheme }.css`;
+      const currentThemeUrl = this.linkTheme.getAttribute('href');
+
+      if (btnThemeUrl === currentThemeUrl){
+        elem.classList.add('working');
+      }
+
+    });
   }
 }
