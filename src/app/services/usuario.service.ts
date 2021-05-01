a7d57bfc1532957224b4684ae7e03f071dc2bbbd
Daniel Schell <schell.daniel@gmail.com>
2021-05-01 01:33:03 -0300

    Login y Auth

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,115 @@
+import { Injectable, NgZone } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap, map, catchError } from "rxjs/operators";
+import { environment } from '../../environments/environment';
+import { Observable, of } from 'rxjs';
+
+import { RegisterForm } from '../interfaces/register-form.interface';
+import { LoginForm } from '../interfaces/login-form.interface';
+import { Router } from '@angular/router';
+
+const base_url = environment.base_url;
+declare const gapi: any;
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  public auth2: any;
+
+  constructor(private http: HttpClient,
+              private router: Router,
+              private ngZone: NgZone) { 
+
+      this.googleInit();
+
+  }
+
+  googleInit(){
+
+    return new Promise( (resolve: any) => {
+
+      console.log('google init');
+
+      gapi.load('auth2', () => {
+        // Retrieve the singleton for the GoogleAuth library and set up the client.
+        this.auth2 = gapi.auth2.init({
+          client_id: '740633358621-732ep9ga7ijbek5cblgorsn78ibtm14e.apps.googleusercontent.com',
+          cookiepolicy: 'single_host_origin',
+        });
+
+        resolve();
+
+      });
+
+
+    })
+}              
+
+  logout(){
+    localStorage.removeItem('token');
+    
+    this.auth2.signOut().then( () => {
+      this.ngZone.run( () => {
+
+        this.router.navigateByUrl('/login');
+
+      })
+    });
+
+  }
+
+  validarToken(): Observable<boolean> {
+    const token = localStorage.getItem('token') || ''
+
+    return this.http.get(`${base_url}/login/renew`, {
+      headers: {
+        'x-token': token
+      }
+    }).pipe(
+        tap( (resp: any) => {
+          localStorage.setItem('token',resp.token)
+        }),
+        map( resp => true ),
+        catchError( error => of(false) )   
+    );
+  }
+
+  crearUsuario(formData: RegisterForm) {
+   
+    return this.http.post(`${ base_url }/usuarios`, formData)
+                .pipe(
+                  tap( (resp:any) =>{
+                    //console.log(resp)
+                    localStorage.setItem('token',resp.token)
+                  })
+                );
+
+  }
+
+  login(formData: LoginForm) {
+   
+    return this.http.post(`${ base_url }/login`, formData)
+                .pipe(
+                  tap( (resp:any) =>{
+                    //console.log(resp)
+                    localStorage.setItem('token',resp.token)
+                  })
+                );
+
+  }
+
+  loginGoogle( token ) {
+   
+    return this.http.post(`${ base_url }/login/google`, { token })
+                .pipe(
+                  tap( (resp:any) =>{
+                    localStorage.setItem('token',resp.token)
+                  })
+                );
+
+  }
+}
+
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+
+// TODO: cambiar el url de base_url en Prod
+
 export const environment = {
-  production: true
+  production: true,
+  base_url: 'http://localhost:3000/api'
 };
